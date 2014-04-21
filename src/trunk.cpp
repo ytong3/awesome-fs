144774b640e6ae4d488d9f69e9e5442d60437667
Aaron Tong <ytong3@utk.edu>
2014-04-20 21:07:31 -0400

    adding more APIs and implementation to trunk

--- src/trunk.cpp
+++ src/trunk.cpp
@@ -1,18 +1,94 @@
 #include "trunk.h"
+#include <iostream>
+#include <algorithm>
+AFS::AFS(int numBytes)
+    :totalDiskSize(100<<20),
+    used_size(0),
+    blockSize(4<<10),
+    numOfBlocks(totalDiskSize/blockSize),
+    numOfInodes(double(numOfBlocks-3)/(1+sizeof(Inode)/double(blockSize))),
+    numOfInodesBlocks(numOfInodes/(blockSize/sizeof(Inode))+numOfInodes%(blockSize/sizeof(Inode))>0?1:0),
+    inodeStartBlcok(3),
+    dataRegionStart(inodeStartBlcok+numOfInodesBlocks);
+    rootDirInode(0),
+    blockMapStartPos(1*blockSize*8),
+    inodeMapStartPos(2*blockSize*8)
+    {
+        create_virtual_disk(numBytes);
+        format();
+        
+        //display summary of Initialization
+        std::cout<<"Initialization succeeded\n";
+        std::cout<<"Virtual Disk size: "<<totalDiskSize<<"M Bytes\n"
+                 <<"Block size: "<<blockSize<<std::endl
+                 <<"# of blocks: "<<numOfBlocks<<std::endl
+                 <<"maximum number of files (inodes): "<<numOfInodes<<std::endl
+                 <<"Data region blocks: "<<numOfBlocks-dataRegionStart<<std::endl;
+    }
 
-AFS::AFS(int numBytes){
-   create_virtual_disk(numBytes);
+bool setBlockMap(size_t pos, bool val){
+   //change blockMap in memory
+   //error checking
+   if (pos>=numOfBlock) return false;
+   blockMap[pos]=val;
+   string strVal('0'+val?1:0);
+   return write_disk(blockMapStartPos+pos,strVal);
 }
 
-void AFS::create_virtual_disk(size_t numBytes){
-    std::ofstream ofs("virtualDisk.D", std::ios_base::binary|std::ios_base::out); 
-    ofs.seekp(((numBytes*8)<<20)-1);
-    ofs.write("",1);
-    ofs.close();
+bool setInodeMap(size_t pos, bool val){
+    if (pos>=numOfInodes) return false;
+    inodeMap[pos] = val;
+    string strVal('0'+val?1:0);
+    return write_disk(inodeMapStartPos+pos, strVal);
 }
+
+size_t get_block_pos(size_t blockNum){
+    return blockSize*8*blockNum;
+}
+
+size_t get_inode_pos(size_t inodeNum){
+    return inodeStartBlcok*blockSize*8+inodeNum*sizeof(Inode)*8;
+}
+
+bool AFS::format(){
+    //display warning msg
+
+    //initialize maps
+    blockMap.resize(numOfBlocks,true);
+    std::fill_n(blockMap.begin(),dataRegionStart,false);
+
+    string blockStr;
+
+    for(int i=0;i<numOfBlocks;i++){
+        blockMapStr.append(1,'0'+blockMap[i]?1:0);
+    }
+    write_disk(blockMapStartPos, blockMapStr);
+
+        
+    inodeMap.resize(numOfInodes,true);
+    inodeMap[0]=false;//reserved for the root dir
+    string inodeMapStr;
+    for(int i=0;i<numOfInodes;i++)
+        inodeMapStr.append(1,'0'+inodeMap[i]?1:0);
+
+    write_disk(inodeMapStartPos,inodeMapStr);
+
+    //create root directory and the corresponding inode
+    
+
+}
+    
+void AFS::create_virtual_disk(size_t numBytes)
+
+    {
+        std::ofstream ofs("virtualDisk.D", std::ios_base::binary|std::ios_base::out); 
+        ofs.seekp(((numBytes*8)<<20)-1);
+        ofs.write("",1);
+        ofs.close();
+    }
 
 uint32_t get_block_pos(size_t blockNum){
-    return blockNum*blockSize;
+    return blockNum*blockSize*8;        //all pos must be in terms of bit
 }
 
 bool write_disk(size_t startPos, string buffer){
--- src/trunk.h
+++ src/trunk.h
@@ -1,27 +1,56 @@
 #include <string>
 #include <fstream>
 #include "inode.h"
+#include <vector>
 
 class AFS {
 private:
     fstream virtualDisk;
-    const double total_size = 100*1<<6*sizeof(char);
+    const string virtualDiskFileName = "virtualDisk.D";
+    const double totalDiskSize = 100*1<<6*sizeof(char);
     double used_size;
 	int blockSize = 4<<10;//in bytes
-	int dataRegionStart;//in blocks
-	int inodeDataStart;//in blocks
-    int rootdirNode;//in blocks
+    const size_t numOfBlocks;
+    const size_t numOfInodes;
+    const size_t numOfInodesBlocks;
+	const size_t dataRegionStart;//in blocks
+	const size_t inodeStartBlcok;//in blocks
+    const size_t rootDirInode;//number of the inode for the root directory
+    const size_t blockMapStartPos;
+    const size_t inodeMapStartPos;
+
+    //bitmap for inode and data region
+    std::vector<bool> blockMap;
+    std::vector<bool> inodeMap;
+    bool setBlockMap(size_t pos, bool val);//this and the next operation couples the memory and disk
+    bool setInodeMap(size_t pos, bool val);
+
 
     void create_virtual_disk(int numBytes);
     AFS(int numBytes);
 
     //utility tools
     //a position (pos) is a 32-bit unsigned number, serving as the absolute offset of the virtual disk.
-    void write_disk(size_t startPos, string buffer);
-    void read_disk(size_t startPos, size_t readSize, string& buffer);
-    size_t get_block_pos(int blockNum); 
+    bool write_disk(size_t startPos, string& buffer);
+    bool read_disk(size_t startPos, size_t readSize, string& buffer);
+    size_t get_block_pos(size_t blockNum);//get start offset of a block by its block number
+    size_t get_inode_pos(size_t inodeNum);//get start offset of a inode by its inode number
     
 
+    //bitmap related
+    void reserve_block(int startNum, int numBlock);
+    void free_block(int startNum, int numBlock);
+    size_t get_free_blocks(int numBlock);
+
+    void reserve_inode(int inodeNum);
+    void free_inode(int inodeNum);
+    size_t get_free_inode();
+
+    //internal utility
+    bool format();
+    
+    
+    
 public:
     bool mkfs();
     bool open(string fileName, string flag);
